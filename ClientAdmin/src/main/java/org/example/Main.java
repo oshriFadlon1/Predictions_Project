f7d73861e1c616a3f0e397bd7fc4631af0dbe333
assasa24 <asafko@mta.ac.il>
2023-09-30 23:58:56 +0300

    started implementing admin client

--- ClientAdmin/src/main/java/org/example/Main.java
+++ ClientAdmin/src/main/java/org/example/Main.java
@@ -1,25 +1,62 @@
 package org.example;
 
 import controllers.AdminMenuController;
+import dtos.DtoActionResponse;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main extends Application {
+    private static OkHttpClient CLIENT = new OkHttpClient();
+    private static String BASE_URL = "http://localhost:8080";
+
     public static void main(String[] args) {
-       launch(args);
+//        Request loginRequest = new Request.Builder().url(BASE_URL + "/admin/login").build();
+//        Call call = CLIENT.newCall(loginRequest);
+//        try{
+//            final Response response = call.execute();
+//            if(response.body().string().equalsIgnoreCase("false")){
+//                return;
+//            }
+//        }
+//        catch(IOException e){
+//            return;
+//        }
+        launch(args);
+    }
+
+    public static /*synchronized*/ OkHttpClient getCLIENT() {
+        return CLIENT;
+    }
+
+    public static void setCLIENT(OkHttpClient CLIENT) {
+        Main.CLIENT = CLIENT;
+    }
+
+    public static String getBaseUrl() {
+        return BASE_URL;
+    }
+
+    public static void setBaseUrl(String baseUrl) {
+        BASE_URL = baseUrl;
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         FXMLLoader loader = new FXMLLoader();
         URL mainFXML = getClass().getResource("/scenes/adminmenu.fxml");
         loader.setLocation(mainFXML);
